7321d686ad9451741f1e3125fe0e65ee556462f8
ahmed-shahawy <shahawya@gmail.com>
2025-03-17 14:41:51 +0300

    Add files via upload

---
+++ pos_bonat_loyalty_18/pos_bonat_loyalty/static/src/overrides/models/control_buttons.js
@@ -0,0 +1,14 @@
+// /** @odoo-module **/
+
+// import { OrderlineNoteButton } from "@point_of_sale/app/screens/product_screen/control_buttons/customer_note_button/customer_note_button";
+// import { SelectPartnerButton } from "@point_of_sale/app/screens/product_screen/control_buttons/select_partner_button/select_partner_button";
+// import { ControlButtons } from "@point_of_sale/app/screens/product_screen/control_buttons/control_buttons";
+// import { patch } from "@web/core/utils/patch";
+// import { BonatCodeButton } from "@pos_bonat_loyalty/bonat_code_button/bonat_code_button";
+// // import { OrderlinePopup } from "@pos_bonat_loyalty/popup/orderline_popup";
+// patch(ControlButtons, {
+//     components: {
+//         ...ControlButtons.components,
+//         BonatcodeButton,
+//     },
+// });
